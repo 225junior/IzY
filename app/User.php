8c48a6754302f561ec1098b64780e0e2d982185f
225junior <225juniorbrindou@gmail.com>
2020-09-27 11:53:29 +0000

    creation de model user

---
+++ app/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use Notifiable,SoftDeletes;
+}
