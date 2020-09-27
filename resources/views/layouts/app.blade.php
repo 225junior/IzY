ecfe6286db0af42c6ad446381b2a41d969b1ee39
225junior <225juniorbrindou@gmail.com>
2020-09-27 14:23:52 +0000

    delete unusefiles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,83 +37,9 @@
         <div class="main_container">
 
             <!-- side bar -->
-                <div class="col-md-3 left_col">
-                    <div class="left_col scroll-view">
-                        <div class="navbar nav_title" style="border: 0;">
-                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <!-- menu profile quick info -->
-                        <div class="profile clearfix">
-                        <div class="profile_pic">
-                            <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-                        </div>
-                        <div class="profile_info">
-                            <span>Welcome,</span>
-                            <h2>{{Auth::user()->name}}</h2>
-                        </div>
-                        </div>
-                        <!-- /menu profile quick info -->
-
-                        <br>
-
-                        <!-- sidebar menu -->
-                        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                            <div class="menu_section active">
-                                <h3>General</h3>
-                                <ul class="nav side-menu" style="">
-                                    <li class="active"><a><i class="fa fa-home"></i> Home</a></li>
-                                    <li>
-                                        <a><i class="fa fa-edit"></i> Ressources <span class="fa fa-chevron-down"></span></a>
-                                        <ul class="nav child_menu">
-                                            <li><a href="form.html">Regions</a></li>
-                                            <li><a href="form_advanced.html">Viles</a></li>
-                                            <li><a href="form_validation.html">Communes</a></li>
-                                            <li><a href="form_wizards.html">Quartiers</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                                        <ul class="nav child_menu">
-                                            <li><a href="general_elements.html">General Elements</a></li>
-                                            <li><a href="media_gallery.html">Media Gallery</a></li>
-                                            <li><a href="typography.html">Typography</a></li>
-                                            <li><a href="icons.html">Icons</a></li>
-                                            <li><a href="glyphicons.html">Glyphicons</a></li>
-                                            <li><a href="widgets.html">Widgets</a></li>
-                                            <li><a href="invoice.html">Invoice</a></li>
-                                            <li><a href="inbox.html">Inbox</a></li>
-                                            <li><a href="calendar.html">Calendar</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        </div>
-                        <!-- /sidebar menu -->
-
-                        <!-- /menu footer buttons -->
-                            <div class="sidebar-footer hidden-small">
-                                <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Settings">
-                                    <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-                                </a>
-                                <a data-toggle="tooltip" data-placement="top" title="" data-original-title="FullScreen">
-                                    <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-                                </a>
-                                <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Lock">
-                                    <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-                                </a>
-                                <a data-toggle="tooltip" data-placement="top" title="" href="login.html" data-original-title="Logout">
-                                    <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                                </a>
-                            </div>
-                        <!-- /menu footer buttons -->
-
-                    </div>
-                </div>
+                @include('layouts.sidebar')
             <!-- fin side bar  -->
+
             <!-- menu -->
                 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                     <div class="container">
