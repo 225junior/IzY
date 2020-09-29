5861cf6842eae63927de81ee1c685f087f958430
225junior <225juniorbrindou@gmail.com>
2020-09-29 13:48:12 +0000

    login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,82 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+  	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <title>{{ config('app.name') }}</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="../vendors/animate.css/animate.min.css" rel="stylesheet">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+</head>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+<body class="login">
+    <div>
+      	<div class="login_wrapper">
+			<div class="animate form login_form">
+				<section class="login_content">
+					<form method="POST" action="{{ route('login') }}">
+						@csrf
+						<h1>Connexion</h1>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+						<div>
+							<input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="E-mail"  name="email" value="{{ old('email') }}" required autocomplete="email" autofocus/>
+							@error('email')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+							@enderror
+						</div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+						<div>
+							<input type="password" name="password" required class="form-control  @error('password') is-invalid @enderror" autocomplete="current-password" placeholder="mot de passe" required="" />
+							@error('password')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+							@enderror
+						</div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+						<div>
+							<button type="submit" class="btn btn-primary submit">Connexion</button>
+							@if (Route::has('password.request'))
+								<a class="reset_pass" href="{{ route('password.request') }}">Mot de passe Oublié ?</a>
+							@endif
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+						</div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+						<div class="clearfix"></div>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+						<div class="separator">
+							<p class="change_link">Vous êtes nouveau ?
+								<a href="{{ route('register') }}" class="to_register"> Créez un compte</a>
+							</p>
+
+							<div class="clearfix"></div>
+							<br />
+
+							<div>
+								<h1><i class="fa fa-paw"></i> 3Dev-Groupe</h1>
+								<p>©2016 All Rights Reserved. 3DevGroupe!  The innovative Startup</p>
+							</div>
+						</div>
+					</form>
+				</section>
+			</div>
+      	</div>
     </div>
-</div>
-@endsection
+</body>
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,6 @@
 
                 <!-- fin topbar -->
 
-            @endauth()
 
 
 			<div class="right_col" role="main" style="min-height: 5183px;">
@@ -84,6 +83,7 @@
 							</div>
 						</div><!-- fin title_right -->
 					</div><!-- fin page-title -->
+					@endauth()
 
 
 
@@ -137,23 +137,5 @@
 
     <!-- Custom Theme Scripts -->
     <script src="{{ asset('build/js/custom.min.js')}}" async></script>
-
-
-    <!-- Datatables -->
-    <!-- <script src="{{ asset('vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-keytable/js/dataTables.keyTable.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js')}}"></script>
-    <script src="{{ asset('vendors/datatables.net-scroller/js/dataTables.scroller.min.js')}}"></script>
-    <script src="{{ asset('vendors/jszip/dist/jszip.min.js')}}"></script>
-    <script src="{{ asset('vendors/pdfmake/build/pdfmake.min.js')}}"></script>
-    <script src="{{ asset('vendors/pdfmake/build/vfs_fonts.js')}}"></script> -->
 </body>
 </html>
