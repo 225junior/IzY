89e54477a78f7f06f383b3316756677946fa9d96
225junior <225juniorbrindou@gmail.com>
2020-10-01 01:34:02 +0000

    villes communes qartiers index

--- resources/views/ressources/villes/index.blade.php
+++ resources/views/ressources/villes/index.blade.php
@@ -2,51 +2,16 @@
 
 @section('content')
 
-<div class="right_col" role="main" style="min-height: 5183px;">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Users <small>Some examples to get you started</small></h3>
-              </div>
-
-              <div class="title_right">
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-secondary" type="button">Go!</button>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="clearfix"></div>
-
             <div class="row">
 
-
-
-
-
-
-
-
-
-
-              <div class="col-md-12 col-sm-12 ">
+			<div class="col-md-8 offset-md-2 col-sm-12  ">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Responsive example<small>Users</small></h2>
+                    <h2>Liste des Villes</h2>
+
                     <ul class="nav navbar-right panel_toolbox">
+						<a href=" {{ Route('villes.create') }}" class="btn btn-success">Creer</a>
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
                       </li>
                       <li><a class="close-link"><i class="fa fa-close"></i></a>
                       </li>
@@ -54,54 +19,70 @@
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
-                      <div class="row">
-                          <div class="col-sm-12">
-                            <div class="card-box table-responsive">
-                    <p class="text-muted font-13 m-b-30">
-                      Responsive is an extension for DataTables that resolves that problem by optimising the table's layout for different screen sizes through the dynamic insertion and removal of columns from the table.
-                    </p>
 
-                    <div id="datatable-responsive_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="datatable-responsive_length"><label>Show <select name="datatable-responsive_length" aria-controls="datatable-responsive" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="datatable-responsive_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="datatable-responsive"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap dataTable no-footer dtr-inline collapsed" cellspacing="0" width="100%" role="grid" aria-describedby="datatable-responsive_info" style="width: 100%;">
+                    <table class="table table-striped">
                       <thead>
-                        <tr role="row"><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 66px;" aria-label="First name: activate to sort column ascending">First name</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 65px;" aria-label="Last name: activate to sort column ascending">Last name</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 150px;" aria-label="Position: activate to sort column ascending">Position</th><th class="sorting_desc" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 66px;" aria-label="Office: activate to sort column ascending" aria-sort="descending">Office</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 25px;" aria-label="Age: activate to sort column ascending">Age</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 61px;" aria-label="Start date: activate to sort column ascending">Start date</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 49px;" aria-label="Salary: activate to sort column ascending">Salary</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 0px; display: none;" aria-label="Extn.: activate to sort column ascending">Extn.</th><th class="sorting" tabindex="0" aria-controls="datatable-responsive" rowspan="1" colspan="1" style="width: 0px; display: none;" aria-label="E-mail: activate to sort column ascending">E-mail</th></tr>
+                        <tr>
+                          <th width="10%">#</th>
+                          <th width="40%">Nom de Villes</th>
+                          <th width="10%">État</th>
+                          <th width="25%"></th>
+                        </tr>
                       </thead>
                       <tbody>
-                      <tr role="row" class="odd">
-                          <td tabindex="0" class="" style="">Airi</td>
-                          <td>Satou</td>
-                          <td>Accountant</td>
-                          <td class="sorting_1">Tokyo</td>
-                          <td>33</td>
-                          <td>2008/11/28</td>
-                          <td style="">$162,700</td>
-                          <td style="display: none;">5407</td>
-                          <td style="display: none;">a.satou@datatables.net</td>
-                        </tr>
-						@foreach ($villes as $ville)
-							<tr role="row" class="even">
-								<td tabindex="0" class="" style="">{{ $ville->libelle }}</td>
-								<td>Winters</td>
-								<td>Accountant</td>
-								<td class="sorting_1">Tokyo</td>
-								<td>63</td>
-								<td>2011/07/25</td>
-								<td style="">$170,750</td>
-								<td style="display: none;">8422</td>
-								<td style="display: none;">g.winters@datatables.net</td>
-                        	</tr>
+					  	@foreach ($villes as $ville)
+							<tr>
+								<th scope="row">{{ $ville->id }}</th>
+								<td><a href="{{ route('villes.show',$ville->id )}}"> {{ $ville->libelle }} </a></td>
+								<th scope="row"> <i class="fa fa-toggle-{{ $ville->active ==1 ?  'on' : 'off'}}"></i> </th>
+								<td>
+                  <button type="button" class="btn btn-danger" data-toggle="modal" data-target=".modal-{{ $ville->id }}">Supprimer</button>
+
+
+                  <div class="modal fade modal-{{ $ville->id }}" tabindex="-1" role="dialog" style="display: none;" aria-hidden="true">
+                    <div class="modal-dialog modal-sm">
+                      <div class="modal-content">
+
+                        <div class="modal-header">
+                          <h4 class="modal-title" id="myModalLabel2">Supprimer  {{ $ville->libelle }}</h4>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          <h4>Attention !</h4>
+                          <p>Voulez vou vraiment supprimer {{ $ville->libelle }} ?</p>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                          <form method="POST" action="{{ route('villes.destroy',$ville->id) }}" class="col-md-5">
+                            @csrf
+                            @method('DELETE')
+                            <input class="btn btn-xs btn-danger" type="submit" value="🗑️">
+                          </form>
+
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+
+								    <a class="btn btn-xs btn-info col-md-3" href="{{ route('villes.edit',$ville->id) }}">🖊️</a>
+								</td>
+							</tr>
 						@endforeach
 
-						</tbody>
-                    </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="datatable-responsive_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="datatable-responsive_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="datatable-responsive_previous"><a href="#" aria-controls="datatable-responsive" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="datatable-responsive" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="datatable-responsive" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="datatable-responsive" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="datatable-responsive" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="datatable-responsive" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="datatable-responsive" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="datatable-responsive_next"><a href="#" aria-controls="datatable-responsive" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
+                      </tbody>
+                    </table>
+
+
+
+
 
 
                   </div>
                 </div>
               </div>
-            </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    	</div>
+    </div>
+</div>
 @endsection
