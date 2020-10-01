9b8c9794cdfd020f66aa4adddb7ec1167c345bcf
225junior <225juniorbrindou@gmail.com>
2020-10-01 13:46:53 +0000

    ajout de middlware guest pour auth.login

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.login');
+Route::middleware('guest')->group(function(){
+
+	Route::get('/', function () {
+		return view('auth.login');
+	});
+
 });
 
 Auth::routes();
