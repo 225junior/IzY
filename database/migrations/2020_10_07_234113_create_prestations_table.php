8dfce3e90c836f60261d68c4e269908418e35aab
225junior <225juniorbrindou@gmail.com>
2020-10-08 01:01:25 +0000

    migration full prestation

---
+++ database/migrations/2020_10_07_234113_create_prestations_table.php
@@ -0,0 +1,101 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrestationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		// creation de Domaines table
+		Schema::create('domaines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('libelle');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+		});
+
+
+
+        Schema::create('services', function (Blueprint $table) {
+	        $table->increments('id');
+			$table->string('libelle');
+			$table->unsignedInteger('domaine_id');
+			$table->foreign('domaine_id')->references('id')->on('domaines')->onDelete('cascade')->onUpdate('cascade');
+			$table->boolean('active')->default(false);
+            $table->timestamps();
+		});
+
+
+
+		Schema::create('tarifications', function (Blueprint $table) {
+	        $table->increments('id');
+			$table->double('montant', 5, 2);
+            $table->timestamps();
+			$table->boolean('active')->default(false);
+		});
+
+
+
+		Schema::create('ligne_tarifs', function (Blueprint $table) {
+	        $table->increments('id');
+			$table->string('libelle');
+			$table->unsignedInteger('tarification_id');
+            $table->foreign('tarification_id')->references('id')->on('tarifications')->onDelete('cascade')->onUpdate('cascade');
+			$table->unsignedInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade')->onUpdate('cascade');
+			$table->boolean('active')->default(false);
+            $table->timestamps();
+		});
+
+
+
+		// creation de Prestataires table
+		Schema::create('prestataires', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenoms');
+			$table->string('tel');
+			$table->date('date_naiss');
+			$table->unsignedInteger('quartier_id');
+            $table->foreign('quartier_id')->references('id')->on('quartiers')->onDelete('cascade')->onUpdate('cascade');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+
+        // creation de la table abonnements
+        Schema::create('abonnements', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('libelle');
+			$table->dateTime('date_debut', 0);
+			$table->dateTime('date_fin', 0);
+            $table->unsignedInteger('prestataire_id');
+            $table->foreign('prestataire_id')->references('id')->on('prestataires')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedInteger('ligne_tarif_id');
+            $table->foreign('ligne_tarif_id')->references('id')->on('ligne_tarifs')->onDelete('cascade')->onUpdate('cascade');
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domaines');
+        Schema::dropIfExists('services');
+        Schema::dropIfExists('tarifications');
+        Schema::dropIfExists('ligne_tarifs');
+        Schema::dropIfExists('prestataires');
+        Schema::dropIfExists('abonnements');
+    }
+}
