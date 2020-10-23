91c15b1b2ee25e95668a837494eb212cac00fb0d
225junior <225juniorbrindou@gmail.com>
2020-10-23 20:27:35 +0000

    travail sur sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 left_col">
-                    <div class="left_col scroll-view">
+    <div class="left_col scroll-view">
                         <div class="navbar nav_title" style="border: 0;">
-                        <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>IzyConnect</span></a>
+                        	<a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>IzyConnect</span></a>
                         </div>
 
                         <div class="clearfix"></div>
@@ -22,30 +22,46 @@
 
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+
                 <div class="menu_section active">
                     <ul class="nav side-menu" style="">
                         <li class="active"><a><i class="fa fa-home"></i> Home</a></li>
                         <li ><a><i class="fa fa-users"></i> Users</a></li>
                         <li ><a href="{{ Route('prestataires.index') }}"><i class="fa fa-briefcase"></i> Prestataires</a></li>
-                        <li ><a href="{{ Route('domaines.index') }}"><i class="fa fa-briefcase"></i> Domaines</a></li>
-                        <li ><a href="{{ Route('cards.index') }}"><i class="fa fa-briefcase"></i> {{ __("Cartes d'identité")}} </a></li>
-                        <li>
-                            <a><i class="fa fa-edit"></i> Ressources <span class="fa fa-chevron-down"></span></a>
-                            <ul class="nav child_menu">
-                                <li><a href="{{ Route('regions.index') }}">Regions</a></li>
-                                <li><a href="{{ Route('villes.index') }}">Viles</a></li>
-                                <li><a href="{{ Route('communes.index') }}">Communes</a></li>
-                                <li><a href="{{ Route('quartiers.index') }}">Quartiers</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+					</ul>
+				</div>
+
+				<div class="menu_section">
+					<h3>Reglages </h3>
+					<li class=""><a><i class="fa fa-list"></i> Ressources <span class="fa fa-chevron-down"></span></a>
+						<ul class="nav side-menu" style="display: none;">
+
+							<li class=""><a>Prestations<span class="fa fa-chevron-down"></span></a>
+								<ul class="nav child_menu" style="display: none;">
+									<li><a href="{{ Route('domaines.index') }}"> Domaines</a></li>
+									<li><a href="{{ Route('domaines.index') }}"> {{__("Carte d'identité")}}</a></li>
+									</li>
+								</ul>
+							</li>
+
+							<li class=""><a>Situation Géographique<span class="fa fa-chevron-down"></span></a>
+								<ul class="nav child_menu" style="display: none;">
+									<li><a href="{{ Route('regions.index') }}">Regions</a></li>
+									<li><a href="{{ Route('villes.index') }}">Viles</a></li>
+									<li><a href="{{ Route('communes.index') }}">Communes</a></li>
+									<li><a href="{{ Route('quartiers.index') }}">Quartiers</a></li>
+									</li>
+								</ul>
+							</li>
+
+						</ul>
+					</li>
+				</div>
+			</div>
             <!-- /sidebar menu -->
 
-                        </div>
+    <!-- /menu footer buttons -->
 
-                        <!-- /menu footer buttons -->
                             <div class="sidebar-footer hidden-small">
                                 <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Settings">
                                     <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
@@ -62,5 +78,5 @@
                             </div>
                         <!-- /menu footer buttons -->
 
-                    </div>
-                </div>
+    </div>
+</div>
